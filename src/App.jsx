83e87e29239fb77de02592e199b01994ac1410b6
Aytekin Ozkan <aytekin7ozkan@gmail.com>
2025-07-01 15:35:58 +0300

    i rendered the list of plants

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
+import { useState } from 'react';
+import ProductList from './components/ProductList';
 import './App.css';
-import AboutUs from './AboutUs';
+import AboutUs from './components/AboutUs';
 
 function App() {
   
