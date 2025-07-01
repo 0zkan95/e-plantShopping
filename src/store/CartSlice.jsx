9db5e7971b2903cf28e0b96c50a868145e93bf63
Aytekin Ozkan <aytekin7ozkan@gmail.com>
2025-07-01 15:48:44 +0300

    cartSlice is coded

--- src/store/CartSlice.jsx
+++ src/store/CartSlice.jsx
@@ -7,13 +7,37 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({
+          name,
+          image,
+          cost,
+          quantity: 1, // Initialize quantity to 1 for new items 
+        });
+      }
     },
+
     removeItem: (state, action) => {
+    
+      state.items = state.items.filter(item => item.name !== name);
+
     },
     updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
 
-    
+      if (existingItem) {
+        if (quantity <= 0) {
+          state.items = state.items.filter(item => item.name !== name);
+        } else {
+          existingItem.quantity = quantity;
+        }
+      }
     },
   },
 });
